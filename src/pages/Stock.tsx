98629b8ec6b5ded207a7abe08fa9b6cbfccd15f8
Ken1o <kenneth.kim.2003.08.23@gmail.com>
2025-12-07 19:23:05 +1300

    making a simple structure for the stock trading sim project description page

--- src/pages/Stock.tsx
+++ src/pages/Stock.tsx
@@ -1,1 +1,26 @@
 import '../styles/Stock.css';
+import TopNav from '../components/TopNav';
+import StockImage from '../assets/StockApp.png';
+import { useNavigate } from 'react-router-dom';
+
+const StockPage: React.FC = () => {
+  const year = new Date().getFullYear();
+  const navigate = useNavigate();
+
+  return (
+    <div className="Stock-root">
+      <TopNav />
+
+      <main className="Stock-main">
+        <div className="Stock-container">
+          <button className="back-btn" onClick={() => navigate('/projects')}>
+            ← Back to Projects
+          </button>
+        </div>
+      </main>
+      <footer className="page-footer">© {year} Jinyoung Kim</footer>
+    </div>
+  );
+}
+
+export default StockPage
